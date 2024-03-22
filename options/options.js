f4ef5f18bec23af916e35e5e6f4990ecf127db85
Crowdbotics <team@crowdbotics.com>
2024-03-22 10:31:21 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mindful-master-47218",
+  url: "https://mindful-master-47218.botics.co",
+  api: "https://mindful-master-47218.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
